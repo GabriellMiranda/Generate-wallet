cfc5b4d38ea449c32df40826ed8512296987ff57
Gabriel Miranda <70436348+GabriellMiranda@users.noreply.github.com>
2024-08-05 19:53:51 -0300

    Update nft.sol

--- src/nft.sol
+++ src/nft.sol
@@ -1,1 +1,32 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    uint256 public nextTokenId;
+    mapping(uint256 => string) private _tokenURIs;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mint(address to, string memory _tokenURI) external onlyOwner {
+        uint256 tokenId = nextTokenId;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        nextTokenId++;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721URIStorage: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        return _tokenURI;
+    }
+}
